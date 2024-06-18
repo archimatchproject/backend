3e4a69d1230ae464e2d05ab7a5054babab5c56b9
ahmedhamila.stage <ahmedhamila.stage@gmail.com>
2024-06-18 13:43:05 +0100

    whitenoise staticfiles

--- project_core/django/production.py
+++ project_core/django/production.py
@@ -3,5 +3,6 @@ from project_core.django.base import *
 DEBUG = True
 
 
-ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['*']
+ALLOWED_HOSTS = ["archimatch-backend.azurewebsites.net",]
+CSRF_TRUSTED_ORIGINS = ["https://archimatch-backend.azurewebsites.net",]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
