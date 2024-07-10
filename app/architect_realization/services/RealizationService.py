fe7014739e1905b65a9a5172b5a65d7c6e58454f
Ghazi-Chaftar <ghazichaftar@gmail.com>
2024-07-10 12:36:17 +0100

    static data api for realization stepper

--- app/architect_realization/services/RealizationService.py
+++ app/architect_realization/services/RealizationService.py
@@ -16,10 +16,15 @@ from rest_framework.exceptions import APIException
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 
+from app.announcement.models.Need import Need
+from app.announcement.serializers.ArchitecturalStyleSerializer import ArchitecturalStyleSerializer
+from app.announcement.serializers.NeedSerializer import NeedSerializer
 from app.architect_realization.models.Realization import Realization
 from app.architect_realization.models.RealizationImage import RealizationImage
 from app.architect_realization.serializers.RealizationSerializer import RealizationOutputSerializer
 from app.architect_realization.serializers.RealizationSerializer import RealizationPOSTSerializer
+from app.core.models.ArchitectSpeciality import ArchitectSpeciality
+from app.core.models.ArchitecturalStyle import ArchitecturalStyle
 from app.users.models.Architect import Architect
 
 
@@ -40,6 +45,7 @@ class RealizationService:
         validated_data = serializer.validated_data
 
         try:
+            needs_data = validated_data.pop("needs")
             user_id = request.user.id
             if not Architect.objects.filter(user__id=user_id).exists():
                 raise NotFound(detail="Architect not found.", code=status.HTTP_404_NOT_FOUND)
@@ -52,6 +58,7 @@ class RealizationService:
                     architect=architect,
                     **validated_data,
                 )
+                realization.needs.set(needs_data)
                 for image in realization_images:
                     RealizationImage.objects.create(
                         realization=realization,
@@ -68,3 +75,55 @@ class RealizationService:
             raise e
         except Exception as e:
             raise APIException(detail=str(e))
+
+    @classmethod
+    def get_architectural_styles(cls):
+        """
+        Retrieves all architectural styles.
+
+        Returns:
+            Response: Response containing list of architectural styles.
+        """
+        try:
+            architectural_styles = ArchitecturalStyle.objects.all()
+            serializer = ArchitecturalStyleSerializer(
+                architectural_styles,
+                many=True,
+            )
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK,
+            )
+        except Exception:
+            return APIException("Error retrieving architectural styles")
+
+    @classmethod
+    def get_architect_speciality_needs(cls, request):
+        """
+        Retrieves needs based on architect speciality.
+
+        Args:
+            architect_speciality_id (int): ID of the architect speciality.
+
+        Returns:
+            Response: Response containing list of needs related to the architect speciality.
+        """
+        try:
+            user_id = request.user.id
+            architect = Architect.objects.get(user__id=user_id)
+
+            needs = Need.objects.filter(architect_speciality_id=architect.architect_speciality.id)
+            serializer = NeedSerializer(needs, many=True)
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK,
+            )
+        except Architect.DoesNotExist:
+            raise NotFound(detail="Architect not found.")
+        except ArchitectSpeciality.DoesNotExist:
+            raise NotFound(detail="No architect speciality found with the given ID")
+        except NotFound as e:
+            raise e
+        except Exception:
+            raise APIException(detail="Error retrieving architect speciality needs")
