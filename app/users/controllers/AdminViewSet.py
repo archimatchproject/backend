90098154f8b41fc6f7b96c109b32b36f2d67fe47
ahmedhamila.stage <ahmedhamila.stage@gmail.com>
2024-07-18 15:16:48 +0100

    update permissions

--- app/users/controllers/AdminViewSet.py
+++ app/users/controllers/AdminViewSet.py
@@ -37,6 +37,8 @@ class AdminViewSet(viewsets.ModelViewSet):
             self.permission_classes = [IsAuthenticated, IsSuperUser]
         elif self.action == "list":
             self.permission_classes = [IsAuthenticated]
+        elif self.action in ["admin_send_reset_password_link","admin_validate_password_token"]:
+            self.permission_classes = []
         else:
             self.permission_classes = [IsAuthenticated]
 
