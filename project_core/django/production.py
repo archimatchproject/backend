86ff05cf597941701ba0eb53cf76e8ca984d2bd7
ahmedhamila.stage <ahmedhamila.stage@gmail.com>
2024-07-23 10:16:43 +0100

    rem yml script

--- project_core/django/production.py
+++ project_core/django/production.py
@@ -9,10 +9,8 @@ DEBUG = True
 
 
 ALLOWED_HOSTS = [
-    "archimatch-backend.azurewebsites.net",
-    "archimatch-backend-test.azurewebsites.net"
+    "archimatch-backend.azurewebsites.net"
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://archimatch-backend.azurewebsites.net",
-    "https://archimatch-backend-test.azurewebsites.net"
+    "https://archimatch-backend.azurewebsites.net"
 ]
