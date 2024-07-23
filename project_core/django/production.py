af1b2e86de14d3fa9e01b4b8acfd93b0ce24651f
ahmedhamilastage <143082508+ahmedhamilastage@users.noreply.github.com>
2024-07-23 09:48:22 +0100

    Update production.py

--- project_core/django/production.py
+++ project_core/django/production.py
@@ -10,7 +10,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "archimatch-backend.azurewebsites.net",
+    "archimatch-backend-test.azurewebsites.net"
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://archimatch-backend.azurewebsites.net",
+    "https://archimatch-backend-test.azurewebsites.net"
 ]
