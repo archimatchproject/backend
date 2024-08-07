be2284f58a5154871cb43bb5e9f6354934818c33
ahmedhamila.stage <ahmedhamila.stage@gmail.com>
2024-08-07 14:56:58 +0100

    update default for date

--- app/cms/models/GuideArticle.py
+++ app/cms/models/GuideArticle.py
@@ -33,8 +33,8 @@ class GuideArticle(BaseModel):
     )
     admin = models.ForeignKey(Admin, on_delete=models.SET_NULL, null=True)
     visible = models.BooleanField(default=False)
-    date = models.DateField(default=timezone.now)
-    rating = models.FloatField(default=0.0,null=True,blank=True)
+    date = models.DateField(default=timezone.now().date, null=True, blank=True)
+    rating = models.FloatField(default=0.0, null=True, blank=True)
 
     def clean(self):
         """
