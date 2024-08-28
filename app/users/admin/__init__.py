88a2592461d4353d4f1cfd9c0a6728a9a0e41fec
Ghazi-Chaftar <ghazichaftar@gmail.com>
2024-08-28 12:49:16 +0100

    add admin model for meeting

--- app/users/admin/__init__.py
+++ app/users/admin/__init__.py
@@ -6,3 +6,4 @@ from app.users.admin.SupplierAdmin import SupplierAdmin
 from app.users.admin.SupplierSpecialityAdmin import SupplierSpecialityAdmin
 from app.users.admin.UnavailabilityAdmin import UnavailabilityAdmin
 from app.users.admin.TimeSlotAdmin import TimeSlotAdmin
+from app.users.admin.MeetingAdmin import MeetingAdmin
