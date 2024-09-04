0af670672460847cecb80e381e33ed7c74a12f97
Ghazi-Chaftar <ghazichaftar@gmail.com>
2024-09-04 15:41:05 +0100

    change supplier cover image

--- app/users/services/SupplierService.py
+++ app/users/services/SupplierService.py
@@ -469,7 +469,7 @@ class SupplierService:
 
             current_cover_image_count = SupplierCoverImage.objects.filter(supplier=supplier).count()
 
-            if current_cover_image_count + len(cover_images) > 3:
+            if current_cover_image_count + len(cover_images) > 4:
                 raise serializers.ValidationError(
                     detail=f"Adding these cover images would exceed the maximum of 3 allowed."
                 )
