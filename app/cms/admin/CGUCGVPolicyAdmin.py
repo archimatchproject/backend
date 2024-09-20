a2e95bf974ded703ca519aef60714eae92e2612e
Ghazi-Chaftar <ghazichaftar@gmail.com>
2024-09-20 16:39:17 +0100

    add cookies policies and cgu policies

--- app/cms/admin/CGUCGVPolicyAdmin.py
+++ app/cms/admin/CGUCGVPolicyAdmin.py
@@ -1,25 +1,25 @@
 """
-Module defining the CookiesPolicyAdmin and related configurations.
+Module defining the CGUCGVPolicyAdmin and related configurations.
 
-This module contains the admin configuration for the CookiesPolicy model,
-providing customization options for managing Cookies policy entries.
+This module contains the admin configuration for the CGUCGVPolicy model,
+providing customization options for managing CGUCGV policy entries.
 """
 
 from django.contrib import admin
 
-from app.cms.models.CookiesPolicy import CookiesPolicy
+from app.cms.models.CGUCGVPolicy import CGUCGVPolicy
 
 
-class CookiesPolicyAdmin(admin.ModelAdmin):
+class CGUCGVPolicyAdmin(admin.ModelAdmin):
     """
-    Admin descriptor for CookiesPolicy model.
+    Admin descriptor for CGUCGVPolicy model.
 
     Provides options for listing, searching, and filtering
-    Cookies policy entries in the Django admin interface.
+    CGUCGV policy entries in the Django admin interface.
     """
 
     list_display = ("admin", "created_at")
     list_filter = ("created_at",)
 
 
-admin.site.register(CookiesPolicy, CookiesPolicyAdmin)
+admin.site.register(CGUCGVPolicy, CGUCGVPolicyAdmin)
