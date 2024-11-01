578c3b1f884d764499db84021498de2d7aa5d5f9
Ghazi-Chaftar <ghazichaftar@gmail.com>
2024-11-01 11:49:53 +0000

    add quote status in serializer

--- app/selection/serializers/SelectionSerializer.py
+++ app/selection/serializers/SelectionSerializer.py
@@ -14,7 +14,7 @@ from app.users.models import Architect
 from app.announcement.models import Announcement
 from app.announcement.serializers.AnnouncementSerializer import AnnouncementSerializer
 from app.users.serializers.ArchitectSerializer import ArchitectSerializer
-from app.selection import SELECTION_STATUS_CHOICES
+from app.selection import SELECTION_STATUS_CHOICES,QUOTE_ACCEPTED,QUOTE_REFUSED,QUOTE_PENDING
 
 
 class SelectionSerializer(serializers.ModelSerializer):
@@ -32,9 +32,51 @@ class SelectionSerializer(serializers.ModelSerializer):
     architect = ArchitectSerializer()
     phase = PhaseSerializer() 
     quotes = QuoteSerializer(many=True, read_only=True)
+    is_last_quote_accepted = serializers.SerializerMethodField()
+    is_last_quote_refused = serializers.SerializerMethodField()
+    last_pending_quote = serializers.SerializerMethodField()
     class Meta:
         model = Selection
-        fields = ['id','announcement', 'architect', 'phase', 'status','quotes','name']
+        fields = ['id', 'announcement', 'architect', 'phase', 'status', 'quotes', 'is_last_quote_accepted', 'is_last_quote_refused', 'name','last_pending_quote']
+
+    def get_is_last_quote_accepted(self, obj):
+        """
+        Checks if the last quote associated with this selection was accepted.
+
+        Args:
+            obj (Selection): The Selection instance.
+
+        Returns:
+            bool: True if the last quote's status is 'Accepted'; False otherwise.
+        """
+        last_quote = obj.quotes.order_by('-created_at').first()
+        return last_quote.status == QUOTE_ACCEPTED if last_quote else False
+
+    def get_is_last_quote_refused(self, obj):
+        """
+        Checks if the last quote associated with this selection was refused.
+
+        Args:
+            obj (Selection): The Selection instance.
+
+        Returns:
+            bool: True if the last quote's status is 'Refused'; False otherwise.
+        """
+        last_quote = obj.quotes.order_by('-created_at').first()
+        return last_quote.status == QUOTE_REFUSED if last_quote else False
+    
+    def get_last_pending_quote(self, obj):
+        """
+        Retrieves the latest quote with a status of 'pending' for this selection.
+
+        Args:
+            obj (Selection): The Selection instance.
+
+        Returns:
+            dict: Serialized data of the last 'pending' quote, or None if no pending quote exists.
+        """
+        last_pending_quote = obj.quotes.filter(status=QUOTE_PENDING).order_by('-created_at').first()
+        return QuoteSerializer(last_pending_quote).data if last_pending_quote else None
 
 class SelectionPostSerializer(serializers.ModelSerializer):
     """
